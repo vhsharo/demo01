c488bc5ecfb070bae3f10ce79348b2f6f7475f22
admin <admin@iMac-de-admin-6.local>
2017-05-19 19:14:03 -0500

    >  agregue un comentario

--- DemoApp/ViewController.swift
+++ DemoApp/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //este
+        //es
+        //un
+        //comentario
     }
 
 
